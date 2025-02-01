4b36e63561cd5fac4e90276e801beebd6e6f56d9
Jessin Sunny <119120870+Jessin-Sunny@users.noreply.github.com>
2025-02-01 22:32:28 +0530

    Add files via upload

---
+++ Turtle Graphics/pentagon.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+x = int(input("Enter the x position(0-400) : "))
+y = int(input("Enter the y position(0-400) : "))
+r = float(input("Enter the r value : "))/255
+g = float(input("Enter the g value : "))/255
+b = float(input("Enter the b value : "))/255
+w = int(input("Enter the width of the square : "))
+t = Turtle()
+t.up()
+t.goto(x,y)
+t.pencolor(r, g, b)
+t.width(w)
+t.down()
+for i in range(5):
+    t.forward(100)
+    t.right(72)
